7ca09da9d7e153899a93ca571ae08a9a2ffd785d
Laura Jimenez <laurajimenez95@gmail.com>
2021-07-07 15:05:13 -0400

    final submission

--- schema.sql
+++ schema.sql
@@ -1,3 +1,11 @@
+-- this is our schema to create the tables - we tested if the data successfully loaded in the tables directly within jupyter notebook.
+
+DROP TABLE "abnb_cleaned",
+			"census_data_cleaned",
+			"restaurant_data_cleaned",
+			"restaurant_data_cleaned",
+			"top_hotels_cleaned",
+			"zipcode_cleaned";
 -- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
@@ -17,8 +25,6 @@ CREATE TABLE "abnb_cleaned" (
     "price" money   NOT NULL,
     "min_nights" int   NOT NULL,
     "availability_365" int   NOT NULL,
-    "Census_Year" int   NOT NULL,
-    "Population" int   NOT NULL,
     CONSTRAINT "pk_abnb_cleaned" PRIMARY KEY (
         "id"
      )
@@ -41,8 +47,6 @@ CREATE TABLE "restaurant_data_cleaned" (
     "State" VARCHAR   NOT NULL,
     "Categories" VARCHAR   NOT NULL,
     "Zip_Code" VARCHAR   NOT NULL,
-    "borough" VARCHAR   NOT NULL,
-    "neighborhood" VARCHAR   NOT NULL,
     CONSTRAINT "pk_restaurant_data_cleaned" PRIMARY KEY (
         "Unique_id"
      )
@@ -52,11 +56,7 @@ CREATE TABLE "top_hotels_cleaned" (
     "Hotel_Name" VARCHAR   NOT NULL,
     "Lat" int   NOT NULL,
     "Lon" int   NOT NULL,
-    "Zip_Code" VARCHAR   NOT NULL,
-    "borough" VARCHAR   NOT NULL,
-    "neighborhood" VARCHAR   NOT NULL,
-    "Census_Year" int   NOT NULL,
-    "Population" int   NOT NULL
+    "Zip_Code" VARCHAR   NOT NULL
 );
 
 CREATE TABLE "zipcode_cleaned" (
@@ -74,9 +74,8 @@ REFERENCES "census_data_cleaned" ("Neighborhood");
 ALTER TABLE "restaurant_data_cleaned" ADD CONSTRAINT "fk_restaurant_data_cleaned_Zip_Code" FOREIGN KEY("Zip_Code")
 REFERENCES "zipcode_cleaned" ("Zip_Code");
 
-ALTER TABLE "top_hotels_cleaned" ADD CONSTRAINT "fk_top_hotels_cleaned_neighborhood" FOREIGN KEY("neighborhood")
-REFERENCES "census_data_cleaned" ("Neighborhood");
+ALTER TABLE "top_hotels_cleaned" ADD CONSTRAINT "fk_top_hotels_cleaned_Zip_Code" FOREIGN KEY("Zip_Code")
+REFERENCES "zipcode_cleaned" ("Zip_Code");
 
 ALTER TABLE "zipcode_cleaned" ADD CONSTRAINT "fk_zipcode_cleaned_neighborhood" FOREIGN KEY("neighborhood")
 REFERENCES "census_data_cleaned" ("Neighborhood");
-
